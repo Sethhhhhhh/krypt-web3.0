9c2a018fe51bdff43a53930f731d3ab64f562fbd
Sethhhhhhh <yohannvvt@gmail.com>
2022-02-22 01:07:07 +0100

    Create smart contract and transaction components.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './index.css'
 import App from './App'
+import { TransactionProvider } from './context/TransactionContext';
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(
+	<TransactionProvider>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</TransactionProvider>,
+	document.getElementById('root')
+);
